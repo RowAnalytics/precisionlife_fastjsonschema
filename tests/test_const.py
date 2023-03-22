0256ddd0acb96d2bfe8f8021f4854814896094a8
Zbigniew Skowron <zbigniew@precisionlife.com>
2023-03-22 18:20:30 +0100

    Improved error messages from const validator.

--- tests/test_const.py
+++ tests/test_const.py
@@ -1,14 +1,18 @@
 import pytest
 
+from precisionlife_fastjsonschema import JsonSchemaValidationException
 
-@pytest.mark.parametrize('value', (
-    'foo',
-    42,
-    False,
-    [1, 2, 3]
-))
-def test_const(asserter, value):
+
+@pytest.mark.parametrize('const, value, expected', [
+    ('foo', 'foo', 'foo'),
+    (42, 42, 42),
+    (False, False, False),
+    ([1, 2, 3], [1, 2, 3], [1, 2, 3]),
+
+    (42, 0, JsonSchemaValidationException('must be const 42 but is: 0', value='{data}', _rendered_path='data', definition='{definition}', rule='const')),
+])
+def test_const(asserter, const, value, expected):
     asserter({
         '$schema': 'http://json-schema.org/draft-06/schema',
-        'const': value,
-    }, value, value)
+        'const': const,
+    }, value, expected)
